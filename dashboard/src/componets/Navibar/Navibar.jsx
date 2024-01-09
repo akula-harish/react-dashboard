8573303b2d225b9915ccdb66f960ce99c25d58a1
BITKEMYERP\harish.akula <harish.akula@bitkemy.com>
2024-01-09 17:10:58 +0530

    #4646 navibar changes and custom style file creation

--- dashboard/src/componets/Navibar/Navibar.jsx
+++ dashboard/src/componets/Navibar/Navibar.jsx
@@ -1,62 +1,89 @@
 import React, { useState } from 'react'
 import "./navibar.css"
 import "../../App.css"
-import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
-import { Sidenav, Nav, Toggle } from 'rsuite';
-import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
-import GroupIcon from '@rsuite/icons/legacy/Group';
-import MagicIcon from '@rsuite/icons/legacy/Magic';
-import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
-import BlockIcon from '@rsuite/icons/Block';
+//import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+//import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import GridViewIcon from '@mui/icons-material/GridView';
+import LayersIcon from '@mui/icons-material/Layers';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import DescriptionIcon from '@mui/icons-material/Description';
+import PivotTableChartIcon from '@mui/icons-material/PivotTableChart';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import ExtensionIcon from '@mui/icons-material/Extension';
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import ReportIcon from '@mui/icons-material/Report';
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import makeStyles from '@mui/styles/makeStyles';
+import { NavibarStyles } from './navibarStyles';
+
+const useStyles = makeStyles(NavibarStyles);
 
 export default function Navibar() {
-    const [expanded, setExpanded] = React.useState(true);
-    const [activeKey, setActiveKey] = React.useState('1');
-    return (
-      <div style={{ width: 240 }}>
-        <Toggle
-          onChange={setExpanded}
-          checked={expanded}
-          checkedChildren="Expand"
-          unCheckedChildren="Collapse"
-        />
-        <hr />
-        <Sidenav expanded={expanded} defaultOpenKeys={['3', '4']}>
-          <Sidenav.Body>
-            <Nav activeKey={activeKey} onSelect={setActiveKey}>
-              <Nav.Item eventKey="1" icon={<DashboardIcon />} href='/'>
-                Dashboard
-              </Nav.Item>
-              <Nav.Item eventKey="2" icon={<GroupIcon />}>
-                User Group
-              </Nav.Item>
-              <Nav.Menu placement="rightStart" eventKey="3" title="Advanced" icon={<MagicIcon />}>
-                <Nav.Item eventKey="3-1">Geo</Nav.Item>
-                <Nav.Item eventKey="3-2">Devices</Nav.Item>
-                <Nav.Item eventKey="3-3">Loyalty</Nav.Item>
-                <Nav.Item eventKey="3-4">Visit Depth</Nav.Item>
-              </Nav.Menu>
-              <Nav.Menu
-                placement="rightStart"
-                eventKey="4"
-                title="Settings"
-                icon={<GearCircleIcon />}
-              >
-                <Nav.Item eventKey="4-1">Applications</Nav.Item>
-                <Nav.Item eventKey="4-2">Channels</Nav.Item>
-                <Nav.Item eventKey="4-3">Versions</Nav.Item>
-                <Nav.Menu eventKey="4-5" title="Custom Action">
-                  <Nav.Item eventKey="4-5-1">Action Name</Nav.Item>
-                  <Nav.Item eventKey="4-5-2">Action Params</Nav.Item>
-                </Nav.Menu>
-              </Nav.Menu>
-                <Nav.Item eventKey='5' href='/error' icon={<BlockIcon/>}>Error Page</Nav.Item>
-            </Nav>
-          </Sidenav.Body>
-          <Sidenav.Toggle expanded={expanded} onToggle={expanded => setExpanded(expanded)} />
-        </Sidenav>
-      </div>
-    );
-  };
-
-
+ 
+    const classes = useStyles();
+    return(
+        <Sidebar className='sidebar_main'>
+            <Menu
+                menuItemStyles={{
+                button: {
+                    [`&.active`]: {
+                    backgroundColor: '#13395e',
+                    color: '#012970',
+                    },
+                },
+                }}
+            >
+                <MenuItem className={classes.menuItem} href='/dashboard'><GridViewIcon/> Dashboard</MenuItem>
+                <SubMenu label = {`${<LayersIcon/>} Components`} className={classes.subMenu}>
+                    <MenuItem>Alerts</MenuItem>
+                    <MenuItem>Accordian</MenuItem>
+                    <MenuItem>Badges</MenuItem>
+                    <MenuItem>Buttons</MenuItem>
+                    <MenuItem>Cards</MenuItem>
+                </SubMenu>
+                <SubMenu label = 'Forms' className={classes.subMenu}>
+                    <MenuItem>Alerts</MenuItem>
+                    <MenuItem>Accordian</MenuItem>
+                    <MenuItem>Badges</MenuItem>
+                    <MenuItem>Buttons</MenuItem>
+                    <MenuItem>Cards</MenuItem>
+                </SubMenu>
+                <SubMenu label = 'Tabels' className={classes.subMenu}>
+                    <MenuItem>Alerts</MenuItem>
+                    <MenuItem>Accordian</MenuItem>
+                    <MenuItem>Badges</MenuItem>
+                    <MenuItem>Buttons</MenuItem>
+                    <MenuItem>Cards</MenuItem>
+                </SubMenu>
+                <SubMenu label = 'Charts' className={classes.subMenu}>
+                    <MenuItem>Alerts</MenuItem>
+                    <MenuItem>Accordian</MenuItem>
+                    <MenuItem>Badges</MenuItem>
+                    <MenuItem>Buttons</MenuItem>
+                    <MenuItem>Cards</MenuItem>
+                </SubMenu>
+                <SubMenu label = 'Icons' className={classes.subMenu}>
+                    <MenuItem>Alerts</MenuItem>
+                    <MenuItem>Accordian</MenuItem>
+                    <MenuItem>Badges</MenuItem>
+                    <MenuItem>Buttons</MenuItem>
+                    <MenuItem>Cards</MenuItem>
+                </SubMenu>
+                <h5 style={{paddingLeft: '20px', fontSize: '14px', color: '#899bbd'}}>PAGES</h5>
+                <MenuItem className={classes.menuItem}>Profile</MenuItem>
+                <MenuItem className={classes.menuItem}>F.A.Q</MenuItem>
+                <MenuItem className={classes.menuItem}>Contact</MenuItem>
+                <MenuItem className={classes.menuItem}>Register</MenuItem>
+                <MenuItem className={classes.menuItem}>Login</MenuItem>
+                <MenuItem className={classes.menuItem} href='/error'>Error</MenuItem>
+                <MenuItem className={classes.menuItem}>Blank</MenuItem>
+            </Menu>
+        </Sidebar>
+    )
+  }
---
+++ dashboard/src/componets/Navibar/navibarStyles.js
@@ -0,0 +1,13 @@
+import createStyles from '@mui/styles/createStyles';
+
+export const NavibarStyles = () => 
+    createStyles({
+        subMenu:{
+            color: '#012970',
+            fontSize: '14px'
+        },
+        menuItem: {
+            color: '#012970',
+            fontSize: '14px'
+        }
+    })

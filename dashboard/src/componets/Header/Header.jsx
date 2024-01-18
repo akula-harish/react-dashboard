04f7e80cad78a94a3d2b7b05aafa9752f9c17534
akula-harish <harishhag8@gmail.com>
2024-01-18 16:27:08 +0530

    #5656 added chat box

--- dashboard/src/componets/Header/Header.jsx
+++ dashboard/src/componets/Header/Header.jsx
@@ -9,8 +9,9 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import "./Header.css" ;
 import Darkmode from './Darkmode';
 import theme from '../../../theme.json'
-function Header() {
+import Chat from '../Chat/Chat';
 
+function Header() {
   const [showData, setShowData] = useState(false);
   const toggleData = () => {
     setShowData(!showData);
@@ -20,21 +21,34 @@ function Header() {
   //   seccolor:"green"
   // };
   //console.log(JSON.stringify(sometheme))
-  console.log(theme,"json")
-  return (
-    <div className='main_header' 
+  // console.log(theme,"json")
+
+  let [openClose, setOpenClose] = useState(true);
+  const menuToggle = () => {
+    return(setOpenClose(!openClose))
+  }
+  
+  const [showChatBox, setShowChatBox] = useState(false);
+  const chatToggle = () => {
+    setShowChatBox(!showChatBox);
+  };
+  return(
+    <>
+      <div className='main_header' 
     //style={{background:theme.pricolor}}
     >
       <header className='nice_header'>
         <div className='nice_left_header'>
-            <div className='logo_content'>
-                <img src={images} className='w-[26px]'/>
-                <h3 className='capitalize font-bold logo'>Nice admin</h3>
-            </div>
+            <a href='/Dashboard'>
+              <div className='logo_content'>
+                  <img src={images} className='w-[26px]'/>
+                  <h3 className='capitalize font-bold logo'>Nice admin</h3>
+              </div>
+            </a>
         </div>
         <div className='nice_middle_header'>
             <ul className='search_content'>
-                <li><MenuIcon className='menu_icon'/></li>
+                <li onClick={menuToggle} style={{cursor: 'pointer'}}><MenuIcon className='menu_icon'/></li>
                 <li>
                     <div className='searchbar'>
                       <SearchIcon className='search_icon'/>
@@ -47,7 +61,7 @@ function Header() {
             <ul className='right_cont'>
            <li> <Darkmode /></li>
              <li><NotificationsNoneIcon onClick={toggleData} className='ntf_icon'/></li>
-                <li><ChatIcon className='right_iocn'/></li>
+                <li><ChatIcon onClick={chatToggle} className='right_iocn'/></li>
               <li> 
                 <div className='right_img'>
                <img src={img} className='img_icon'/>
@@ -67,7 +81,11 @@ function Header() {
        </div>
       </header>
     </div>
-  )
+      <div className='chatbox_main'><Chat showChatBox={showChatBox}/></div>
+    </>
+  ) 
+
 }
+
 
 export default Header

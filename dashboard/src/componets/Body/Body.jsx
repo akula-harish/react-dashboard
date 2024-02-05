75c8f2b18f770e8a0d4189149a0aa46f975ceaee
akula-harish <harishhag8@gmail.com>
2024-02-05 11:20:33 +0530

    #5656 body and url component changes

--- dashboard/src/componets/Body/Body.jsx
+++ dashboard/src/componets/Body/Body.jsx
@@ -4,13 +4,39 @@ import "./body.css"
 import Dashboard from '../Dashboard/Dashboard'
 import Error from '../Error/Error'
 import Register from '../Register/Register'
+import Contact from '../contact/Contact'
+let ROUTE_URLS = [
+  {
+    url : 'dashboard',
+    component: Dashboard
+  } ,
+  {
+    url : 'error',
+    component: Error
+  } ,
+  {
+    url:'register',
+    component:Register
+  },
+]
+
 export default function Body() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path = "/dashboard" element = {<Dashboard/>}/>
+          {
+          // ROUTE_URLS.map((item ,i) => (
+          // <Route   key={item.url} path = {item.url} element = {<item.component/> }/>
+          // ))
+
+          ROUTE_URLS.map((item ,i) => (
+            <Route   key={item.url} path = {item.url} element = {<item.component/> }/>
+            ))
+            
+          }
+            {/* <Route path = "/dashboard" element = {<Dashboard/>}/>
             <Route path = "/error" element = {<Error/>}/>
-            <Route path= "/Register" element = {<Register/>}/>
+            <Route path= "/Register" element = {<Register/>}/> */}
         </Routes>
     </BrowserRouter>
   )
---
+++ dashboard/src/utils/URLService.js
@@ -0,0 +1,14 @@
+// export const apiServices = {
+//     dasboard: {
+//         fetchDashboardDetails :'//'
+
+//     },
+//     profile :{
+//         getUserDetails : '',
+//         saveUserDetails :''
+//     },
+
+// }
+
+export const DASHBOARD_DETAILS = '';
+export const PROFILE_FETCH_USERDETAILS = '';

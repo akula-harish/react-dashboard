03632cba76be2ecd83d24a240e5bd0e44a0a5fe9
rukmini.b <rukmini.bongu@bitkemy.com>
2024-02-08 14:45:27 +0530

    #0000:alert page design

--- dashboard/src/componets/Alert/Alert.jsx
+++ dashboard/src/componets/Alert/Alert.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
-
+import Alerts from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import './Alert.css'
 function Alert() {
   return (
-    <div className='container'>
-      <p className='text-danger'>alert Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, aperiam.</p>
-    </div>
+    <ul className='alert_head'>
+     <li>
+     <Stack sx={{ width: '100%' }} spacing={2} className='alert_box'>
+      <Alerts severity="success" >This is a success Alert.</Alerts>
+      <Alerts severity="info">This is an info Alert.</Alerts>
+      <Alerts severity="warning">This is a warning Alert.</Alerts>
+      <Alerts severity="error" >This is an error Alert.</Alerts>
+    </Stack>
+     </li>
+     <li>
+     <Stack sx={{ width: '100%' }} spacing={2} className='alert_box'>
+      <Alerts severity="success" variant="filled">This is a success Alert.</Alerts>
+      <Alerts severity="info" variant="filled">This is an info Alert.</Alerts>
+      <Alerts severity="warning" variant="filled">This is a warning Alert.</Alerts>
+      <Alerts severity="error" variant="filled">This is an error Alert.</Alerts>
+     
+    </Stack>
+     </li>
+     <li>
+     <Stack sx={{ width: '100%' }} spacing={2} className='alert_box'>
+      <Alerts severity="success" variant="outlined" className='test'>This is a success Alert.</Alerts>
+      <Alerts severity="info" variant="outlined" className='test'>This is an info Alert.</Alerts>
+      <Alerts severity="warning" variant="outlined" className='test'>This is a warning Alert.</Alerts>
+      <Alerts severity="error" variant="outlined" className='test'>This is an error Alert.</Alerts>
+      
+    </Stack>
+     </li>
+     <li>
+     <Stack sx={{ width: '100%' }} spacing={2} className='alert_box'>
+      <Alerts severity="success" >This is a success Alert.</Alerts>
+      <Alerts severity="info">This is an info Alert.</Alerts>
+      <Alerts severity="warning">This is a warning Alert.</Alerts>
+      <Alerts severity="error" >This is an error Alert.</Alerts>
+    </Stack>
+     </li>
+    </ul>
   )
 }
-
 export default Alert
